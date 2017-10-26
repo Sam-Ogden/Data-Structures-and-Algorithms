6e243bc8dcbdb67aa9db0198e47bdd2e90198a8b
Samuel Ogden <samuelogden@Samuels-MacBook-Air.local>
2017-10-26 19:36:17 +0100

    added comments to knapsack

--- SpecialistAlgorithms/Knapsack.java
+++ SpecialistAlgorithms/Knapsack.java
@@ -1,3 +1,7 @@
+/*
+    Returns the max value you can get with weight limit W.
+    input: n items with weight Wi and value Vi.
+*/
 public class Knapsack {
     static int[] values = new int[] {894, 260, 392, 281, 27};
     static int[] weights = new int[] {8, 6, 4, 0, 21};
@@ -8,12 +12,19 @@ public class Knapsack {
     }
     
     private static int knapsack(int i, int W) {
-        if (i < 0) {
+        //no more items to check or no more weight
+        if (i < 0 || W == 0) {
             return 0;
         }
+        //weight of current item to big for left over weight
+        //return the next item down with left over weight
         if (weights[i] > W) {
             return knapsack(i-1, W);
         } else {
+            /*  
+                Take max of: the next item back with left over weight (exclude curr item) or 
+                the previous one back with weight - current items weight (include curr item)
+            */
             return Math.max(knapsack(i-1, W), knapsack(i-1, W - weights[i]) + values[i]);
         }
     }
